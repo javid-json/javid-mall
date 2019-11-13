d35935b67a486050a5ca0204a6cb6289f6fb4d5a
CRP_GUIJW0517 <424569775@qq.com>
2019-11-13 14:33:39 +0800

    启动类

--- mall-master/src/main/java/pers/javid/mall/MallMasterAplication.java
+++ mall-master/src/main/java/pers/javid/mall/MallMasterAplication.java
@@ -3,11 +3,13 @@ package pers.javid.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("pers.javid.mall.dao")
-public class MallAplication {
+@ServletComponentScan(basePackages = "pers.javid.mall.filter")
+public class MallMasterAplication {
     public static void main(String[] args){
-        SpringApplication.run(MallAplication.class,args);
+        SpringApplication.run(MallMasterAplication.class,args);
     }
 }
